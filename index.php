896a2c99f3e277383526a5832523c95562a4aabb
nalskii <103160634+nalskii@users.noreply.github.com>
2022-12-12 09:52:59 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+function romawi($angka) {
+    $map = array('M' => 1000, 'CM' => 900, 'D' => 500, 'CD' => 400, 'C' => 100, 'XC' => 90, 'L' => 50, 'XL' => 40, 'X' => 10, 'IX' => 9, 'V' => 5, 'IV' => 4, 'I' => 1);
+    $val = '';
+    while ($angka > 0) {
+        foreach ($map as $rom => $int) {
+            if ($angka >= $int) {
+                $angka -= $int;
+                $val .= $rom;
+                break;
+            }
+        }
+    }
+    return $val;
+}
+echo(romawi(20))
+?>
